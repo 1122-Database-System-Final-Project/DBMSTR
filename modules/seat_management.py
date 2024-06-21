c2586f7044610d2abb0f402c6f5ee8f3da0a7ef7
zhanzhiya <vivianvivin.vivianvivin55@gmail.com>
2024-06-21 12:26:10 +0800

    vivian

--- modules/seat_management.py
+++ modules/seat_management.py
@@ -6,7 +6,7 @@ from datetime import datetime
 路徑可能需要根據作業系統調整
 '''
 BASE_DIRECTORY = os.path.abspath(os.path.dirname(__file__)) # 取得當前檔案所在目錄的絕對路徑
-DATABASE = os.path.join(BASE_DIRECTORY, '../database/train_booking.db') # 導航到目錄位置
+DATABASE = os.path.join(BASE_DIRECTORY, '../database/database.db') # 導航到目錄位置
 
 # 獲取指定班次的所有空座位
 def get_all_available_seats_by_train_id(train_id):
@@ -15,8 +15,8 @@ def get_all_available_seats_by_train_id(train_id):
     cursor.execute('''
                    SELECT seat.seat_id, seat.seat_type, seat.car_id 
                    FROM seat JOIN car ON seat.car_id = car.car_id
-                   WHERE car.train_id = ? AND seat.occupied = 0
-                   ''', (train_id))
+                   WHERE car.train_id = ? AND seat.occupied = ' n'
+                   ''', (train_id,))
     seats = cursor.fetchall()
     connection.close()
     return seats

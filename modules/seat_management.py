768ea15b57cc838ecc1d08eea234f72ec02576b3
zhanzhiya <99878425+zhanzhiya@users.noreply.github.com>
2024-06-23 18:58:35 +0800

    seat (#30)
    * vivian
    
    * Vivian
    
    * Vivian
    
    * vivian
    
    * vivian
    
    * select.seat
    
    * Update app.py
    
    * vivian
    
    * seat
    
    * Update seat_management.py
    
    * Update app.py
    
    * Update app.py
    
    * seat db
    
    * seat

--- modules/seat_management.py
+++ modules/seat_management.py
@@ -14,8 +14,8 @@ def get_all_available_seats_by_train_id(train_id):
     cursor = connection.cursor()
     cursor.execute('''
                    SELECT seat.seat_id, seat.seat_type, seat.car_id 
-                   FROM seat JOIN car ON seat.car_id = car.car_id
-                   WHERE car.train_id = ? AND trim(seat.occupied) = 'n'
+                   FROM seat
+                   WHERE (seat.seat_id/1000) = ? AND trim(seat.occupied) = 'n'
                    ''', (train_id,))
     seats = cursor.fetchall()
     connection.close()

36d5887acc4f026bf307842134f8fff8deb8b50c
ching11720 <jasmine910402614@gmail.com>
2024-06-23 11:20:28 +0800

    fix the blank problem in db and search

--- modules/search_train.py
+++ modules/search_train.py
@@ -9,24 +9,35 @@ FROM stopped_by sb1
 JOIN stopped_by sb2 ON sb1.train_id = sb2.train_id
 JOIN station s1 ON sb1.station_id = s1.station_id
 JOIN station s2 ON sb2.station_id = s2.station_id
-WHERE s1.station_name = ?
-  AND s2.station_name = ?
-  AND sb1.departure_time BETWEEN ? AND ?
-ORDER BY sb1.departure_time;
+WHERE trim(s1.station_name) = ?
+  AND trim(s2.station_name) = ?
+  AND trim(sb1.departure_time) BETWEEN ? AND ?
+  AND trim(sb2.arrival_time) > trim(sb1.departure_time)
+ORDER BY trim(sb1.departure_time);
 """
 
-def train_query(departure, destination, date, time1, time2):
+def get_stations():
     try: 
-        datetime1 = datetime.strptime(date + ' ' + time1, '%Y-%m-%d %H:%M:%S')
-        datetime2 = datetime.strptime(date + ' ' + time2, '%Y-%m-%d %H:%M:%S')
         conn = sqlite3.connect(db)
         cursor = conn.cursor()
-        cursor.execute(search_query, (departure, destination, datetime1, datetime2))
+        cursor.execute("SELECT station_name FROM station;")
         result = cursor.fetchall()
+        cursor.close()
+        conn.close()
         return result
     except sqlite3.Error as e:
         print("SQL error: ", e)
         return None
-    finally:
-        if conn:
-            conn.close()
+
+def train_query(departure, destination, time1, time2):
+    try: 
+        conn = sqlite3.connect(db)
+        cursor = conn.cursor()
+        cursor.execute(search_query, (departure, destination, time1, time2))
+        result = cursor.fetchall()
+        cursor.close()
+        conn.close()
+        return result
+    except sqlite3.Error as e:
+        print("SQL error: ", e)
+        return None
